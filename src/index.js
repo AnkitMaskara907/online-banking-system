390ad163193587ec9dcda22332ee8ee832a5550d
AnkitMaskara907 <ankitmaskara907@gmail.com>
2023-09-20 21:07:28 +0530

    Added Changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const cors = require('cors');
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// App.use(cors(corsOptions));
 root.render(
   <React.StrictMode>
     <App />
